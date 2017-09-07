9ace32ef88113099a0991c86e14e445b7f8665b6
jaimeborjas <jaime.borjas@hotmail.com>
2017-09-07 13:42:26 -0500

    titulo

--- js/app.js
+++ js/app.js
@@ -1,1 +1,56 @@
+// numeros aleatorios
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+// cambio de color del titulo
+ function colorBlink(selector) {
+  $(selector).animate({
+    opacity: '1',
+  },
+  {
+    step: function() {
+      $(this).css('color', '#DCFF0E');
+    },
+    queue: true
+  }
+  )
+  .animate({
+    opacity: '1'
+  },
+  {
+    step: function() {
+      $(this).css('color', 'red');
+    },
+    queue: true
+  }, 600
+  )
+  .delay(1000)
+  .animate({
+    opacity: '1'
+  },
+  {
+    step: function() {
+      $(this).css('color', '#DCFF0E');
+    },
+    queue: true
+  }
+  )
+  .animate({
+    opacity: '1'
+  },
+  {
+    step: function() {
+      $(this).css('color', 'purple')
+      colorBlink('h1.main-title');
+    },
+    queue: true
+  }
+  );
+}
+// columnas
+
 
